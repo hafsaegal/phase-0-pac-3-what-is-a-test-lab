df52f31ed618ab35ccc560b0c3531fbeaf23a06a
Hafsa Egal <hafsaegal915@gmail.com>
2025-02-25 07:27:06 +0300

    test1

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,16 +4,16 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+    it('returns "Joe"', () => {
+      expect(name).toEqual("Joe");
     });
   });
 
-  describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
+  describe("Height", () => 
+    (is less than 40 and greater than 0, () => {
       expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
-    });
+      expect(70).toBeLessThan(40);
+    }));
   });
 
   describe("Message", () => {
